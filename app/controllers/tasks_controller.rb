cf8382c34be9bba14e1352b0f22e40003584d315
jiro.kirameki <mm134m@gmail.com>
2017-04-21 04:51:49 +0000

    shusei

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
     before_action :set_task, only: [:edit, :update, :destroy]
     before_action :require_user_logged_in
+    before_action :correct_user, only: [:destroy, :edit]
    def create
     @task = current_user.tasks.build(task_params)
     if @task.save
